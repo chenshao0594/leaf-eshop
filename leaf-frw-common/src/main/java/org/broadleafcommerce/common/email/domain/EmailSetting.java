59cb683a0be60ee1f082a28fd6ef02d3fa6d257f
陈少钦 <chenshaoqin@chenshaoqindeMacBook-Pro.local>
2017-04-08 23:42:28 +0800

    init ...

---
+++ leaf-frw-common/src/main/java/org/broadleafcommerce/common/email/domain/EmailSetting.java
@@ -0,0 +1,12 @@
+package org.broadleafcommerce.common.email.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMAIL_SETTING")
+public class EmailSetting implements Serializable{
+
+}
---
+++ leaf-frw-core/leaf-profile/src/main/java/org/broadleafcommerce/profile/core/domain/ContactPerson.java
@@ -0,0 +1,5 @@
+package org.broadleafcommerce.profile.core.domain;
+
+public class ContactPerson {
+
+}
